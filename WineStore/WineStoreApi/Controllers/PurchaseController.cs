950b1ff02429e71fd0b9d9295f44f234f8d63031
Daniel Stocker <mail@danielstocker.net>
2017-08-21 21:57:15 +0100

    renaming controllers adding trolley API adding controllers to web front end

--- WineStore/WineStoreApi/Controllers/PurchaseController.cs
+++ WineStore/WineStoreApi/Controllers/PurchaseController.cs
@@ -7,35 +7,23 @@ using Microsoft.AspNetCore.Mvc;
 namespace WineStoreApi.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class PurchaseController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/values/5
+        // GET api/purchase/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/purchase
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
+        // DELETE api/purchase/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- WineStore/WineStoreTrolley/Controllers/TrolleyController.cs
+++ WineStore/WineStoreTrolley/Controllers/TrolleyController.cs
@@ -4,38 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WinStoreInventory.Controllers
+namespace WineStoreTrolley.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class TrolleyController : Controller
     {
-        // GET api/values
+        // GET api/trolley
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[];
         }
 
-        // GET api/values/5
+        // GET api/trolley/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "empty";
         }
 
-        // POST api/values
+        // POST api/trolley
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/trolley/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/trolley/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
